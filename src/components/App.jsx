33f8e3024e106c2df8376380d86cc2945e8127bd
Tamerlan <tamerlan.musaev@mail.ru>
2022-03-21 16:03:24 +0300

    fist-commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./footer/Footer";
+import Header from "./header/Header";
+import Main from "./main/Main";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
