d012149dced3726baab16399d38a9c39700fca6b
Islam Komurzoeve <jackcoul0677l@gmailr.com>
2022-03-22 17:06:55 +0300

    add reducer busy

--- src/redux/configueStore.js
+++ src/redux/configueStore.js
@@ -1,11 +1,13 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import user from "./features/user";
 import films from "./features/films";
+import busy from "./features/busy";
 const { default: thunk } = require("redux-thunk");
 
 const combineRouter = combineReducers({
   user,
   films,
+  busy,
 });
 
 const store = createStore(combineRouter, applyMiddleware(thunk));
