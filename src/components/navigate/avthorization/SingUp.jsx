5a3287e5e52c648b01efe7d4dcd6e44819250a7b
Ibragim <dragunov.lion@gmail.com>
2022-03-22 13:55:05 +0300

    Ибрагим: сделал регистрацию и авторизацию

--- src/components/navigate/avthorization/SingUp.jsx
+++ src/components/navigate/avthorization/SingUp.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../../../redux/features/user";
 import styles from "../Navigate.module.css";
 
 const SingUp = () => {
+  const dispatch = useDispatch()
+  const [login, setLogin]= useState()
+  const [password, setPassword]= useState()
+  const [name, setName]= useState()
+
+  const handleRegister = (name,login,password)=>{
+    dispatch(registerUser(name,login,password))
+  }
+
   return (
     <div>
       <div className={styles.singUp}>
 
-        <input placeholder="Введите никнейм.." type="text" />
-        <input placeholder="Введите email.." type="text" />
-        <input placeholder="Введите пароль.." type="text" />
-        <button> Зарегистрироваться</button>
+        <input placeholder="Введите никнейм.." type="text" value={name} onChange={(e)=> setName(e.target.value)} />
+        <input placeholder="Введите email.." type="text" value={login} onChange={(e)=> setLogin(e.target.value)} />
+        <input placeholder="Введите пароль.." type="text" value={password} onChange={(e)=> setPassword(e.target.value)} />
+        <button onClick={()=> handleRegister(name,login,password)}> Зарегистрироваться</button>
       </div>
     </div>
   );
